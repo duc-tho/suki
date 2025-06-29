fd4095ba17dd12048a404fbf94d5b78257984447
thopk007113 <thopk007113@gmail.com>
2025-06-29 11:11:02 +0000

    feat(tikdown): improve tiktok downloader ux

--- resources/js/components/common/tikdown.tsx
+++ resources/js/components/common/tikdown.tsx
@@ -5,6 +5,7 @@ import { toast } from 'sonner'
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
 import { Button } from '../ui/button'
 import { Input } from '../ui/input'
+import { Skeleton } from '../ui/skeleton'
 
 export const tikdownRoute: Route = {
    name: 'Công cụ tải Video Tiktok',
@@ -18,6 +19,7 @@ export default function Tikdown() {
    const [urls, setUrls] = useState<string[]>([])
    const [selectedUrl, setSelectedUrl] = useState<string | null>(null)
    const [videoInfo, setVideoInfo] = useState<any>(null)
+   const [isProcessing, setIsProcessing] = useState(false)
 
    const handleUrlChange = (event: ChangeEvent<HTMLInputElement>) => {
       const value = event.target.value
@@ -48,6 +50,9 @@ export default function Tikdown() {
    const fetchVideoInfo = async () => {
       if (!selectedUrl) return
 
+      setVideoInfo(null)
+      setIsProcessing(true)
+
       try {
          const response = await fetch(`/api/tikdown?url=${encodeURIComponent(selectedUrl)}`)
 
@@ -63,12 +68,22 @@ export default function Tikdown() {
          toast('Lỗi tải video!', {
             description: 'Đã có lỗi xảy ra khi tải video, vui lòng thử lại sau.',
          })
+      } finally {
+         setIsProcessing(false)
       }
    }
 
-   const handleDownload = async () => {
+   const handleClear = () => {
+      setIsProcessing(false)
+      setSelectedUrl(null)
+      setVideoInfo(null)
+      setUrlInput('')
+      setUrls([])
+   }
+
+   const handleDownload = async (noWatermark: boolean = true) => {
       axios({
-         url: videoInfo?.nwm,
+         url: noWatermark ? videoInfo?.nwm : videoInfo?.wm,
          method: 'GET',
          responseType: 'blob',
          headers: {
@@ -138,23 +153,49 @@ export default function Tikdown() {
                placeholder="Dán video link vào đây"
                value={urlInput}
                onChange={handleUrlChange}
+               disabled={isProcessing}
             />
          </div>
          <div className="flex w-full gap-2">
-            <Button variant="outline" className="w-25 cursor-pointer backdrop-blur">
+            <Button
+               variant="outline"
+               className="w-25 cursor-pointer backdrop-blur"
+               disabled={isProcessing}
+               onClick={handleClear}
+            >
                Xóa
             </Button>
-            <Button className="flex-1 cursor-pointer backdrop-blur">Tìm Kiếm</Button>
+            <Button className="flex-1 cursor-pointer backdrop-blur" disabled={isProcessing}>
+               Tìm Kiếm
+            </Button>
             <Button
                variant="outline"
                className="w-25 cursor-pointer backdrop-blur"
                onClick={handlePaste}
+               disabled={isProcessing}
             >
                Dán Link
             </Button>
          </div>
          <div className="bg-background/20 flex flex-1 flex-col items-center justify-center gap-2 rounded p-2">
-            {!videoInfo ? (
+            {isProcessing ? (
+               <>
+                  <div className="flex w-full items-center gap-2">
+                     <Skeleton className="bg-secondary h-8 w-8 rounded-full" />
+                     <Skeleton className="bg-secondary h-full flex-1" />
+                  </div>
+                  <div className="bg-background/50 relative flex w-full flex-1 items-center justify-center rounded backdrop-blur">
+                     <Skeleton className="bg-secondary h-full w-full" />
+                  </div>
+                  <div className="w-full">
+                     <Skeleton className="bg-secondary mb-2 h-6 w-full" />
+                     <Skeleton className="bg-secondary h-6 w-full" />
+                  </div>
+                  <div className="w-full">
+                     <Skeleton className="bg-secondary h-10 w-full" />
+                  </div>
+               </>
+            ) : !videoInfo ? (
                <div className="text-muted-foreground text-center">
                   Hãy dán link video Tiktok hoặc Douyin vào ô trên và nhấn "Tìm Kiếm"
                </div>
@@ -173,6 +214,13 @@ export default function Tikdown() {
                         controls
                         autoPlay
                         loop
+                        onLoadStart={(event) => {
+                           if (!event.currentTarget) {
+                              return
+                           }
+
+                           event.currentTarget.volume = 0.4
+                        }}
                         src={videoInfo.nwm}
                      ></video>
                   </div>
@@ -185,18 +233,36 @@ export default function Tikdown() {
                      )}
                      <div className="w-full overflow-hidden text-ellipsis whitespace-nowrap">
                         <span className="font-bold">Liên kết: </span>
-                        <a href="https://img.pikbest.com/10/73/95/12BpIkbEsTfeM.mp4">
-                           https://img.pikbest.com/10/73/95/12BpIkbEsTfeM.mp4
+                        <a href={selectedUrl ?? '#'} target="_blank">
+                           {selectedUrl ?? 'Chưa có thông tin!'}
                         </a>
                      </div>
                   </div>
-                  <div className="w-full">
-                     <Button
-                        className="w-full cursor-pointer backdrop-blur"
-                        onClick={handleDownload}
-                     >
-                        Tải xuống
-                     </Button>
+                  <div className="flex w-full flex-wrap gap-2">
+                     {videoInfo?.nwm || videoInfo?.wm ? (
+                        <>
+                           {videoInfo?.wm && (
+                              <Button
+                                 className="grow cursor-pointer backdrop-blur"
+                                 onClick={() => handleDownload(false)}
+                              >
+                                 Tải video có nguồn
+                              </Button>
+                           )}
+                           {videoInfo?.nwm && (
+                              <Button
+                                 className="grow cursor-pointer backdrop-blur"
+                                 onClick={() => handleDownload()}
+                              >
+                                 Tải video không nguồn
+                              </Button>
+                           )}
+                        </>
+                     ) : (
+                        <Button className="grow backdrop-blur" disabled>
+                           Rất tiếc hiện không thể tải được video này!
+                        </Button>
+                     )}
                   </div>
                </>
             )}
